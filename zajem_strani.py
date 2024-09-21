83e57d2ee199f83cb69adb6dc3e666801c0baab5
Jure Kraševec <jk82638@student.uni-lj.si>
2024-09-21 12:12:08 +0200

    Popravljen zajem strani

--- zajem_strani.py
+++ zajem_strani.py
@@ -1,41 +1,51 @@
 import requests
 import os
-
+import csv
+import json
+from bs4 import BeautifulSoup
 
 #url spletne strani
-sistat_url = "https://pxweb.stat.si/SiStatData/pxweb/sl/Data/-/2640010S.px/table/tableViewLayout1/"
+brstats_url = "https://www.basketball-reference.com/leagues/NBA_2024_per_game.html"
 #datoteka, kjer bomo shranili html spletne strani
-sistat_html = "stran.html"
+brstats_html = "stran.html"
 #datoteka, kjer bomo shranili podatke iz spletne strani v csv
-sistat_csv = "podatki.csv"
+brstats_csv = "podatki.csv"
 #mapa s podatki
 direktorij = "podatki"
 
 
-def url_v_niz(url):
+#funkcija, ki prejme url spletne strani in naredi datoteko z HTML kodo spletne strani
+def shrani_url_v_html_datoteko(url, datoteka):
     try:
         vsebina = requests.get(url)
         if vsebina.status_code == 200:
-            return vsebina.text
+            #return vsebina.text
+            with open(datoteka, "w", encoding="utf-8") as dat:
+                dat.write(vsebina.text)
+                print("Datoteka je shranjena.")
     except:
         print(f"Napaka pri nalaganju URL-ja {url}")
         return None
     return vsebina.text
 
 
-def url_v_html_datoteko(text, mapa, datoteka):
-    os.makedirs(mapa, exist_ok=True)
-    path = os.path.join(mapa, datoteka)
-    with open(path, "w", encoding="utf-8") as dat:
-         dat.write(text)
-
-
+#prebere niz z vsebino datoteke
 def preberi_besedilo_iz_html(mapa, datoteka):
     path = os.path.join(mapa, datoteka)
     with open(path, "r", encoding="utf-8") as dat:
         return dat.read()
     
 
+#funkcija, ki prejme datoteko z neurejeno html kodo in jo uredi
+def uredi_html(datoteka):
+    with open(datoteka, "r", encoding="utf-8") as dat:
+        soup = BeautifulSoup(dat, "html.parser")
+        urejen_html = soup.prettify()
+        with open(datoteka, "w", encoding="utf-8") as dat:
+            dat.write(urejen_html)
+            print("Urejena HTML koda je bila prepisana.")
 
-url_v_niz(sistat_url) 
-url_v_html_datoteko(url_v_niz(sistat_url), direktorij, sistat_html)
+
+shrani_url_v_html_datoteko(brstats_url, brstats_html)
+uredi_html(brstats_html)
+
